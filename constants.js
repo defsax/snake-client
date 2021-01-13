26ed6762cc01e815bccb6d623807e9bb6ce78de7
Perry Defayette <defayetteperry@gmail.com>
2021-01-13 18:42:22 +0000

    Added Constants file to hold configs.

---
+++ constants.js
@@ -0,0 +1,23 @@
+//vas network
+const IP = '135.23.222.131';
+const PORT = 50542;
+const NAME = 'Name: P3R';
+
+const MOVE_KEYS = {
+  up: 'w',
+  down: 's',
+  left: 'a',
+  right: 'd'
+};
+
+//local
+//const IP = '10.0.2.15';
+//const PORT = 50541;
+
+module.exports = {
+  IP,
+  PORT,
+  NAME,
+
+  MOVE_KEYS
+};
