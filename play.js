427fd6f2cff9227364b60bbf75303de84de9855d
Perry Defayette <defayetteperry@gmail.com>
2021-01-13 18:04:01 +0000

    Seperated input functions into input.js

--- play.js
+++ play.js
@@ -1,22 +1,7 @@
 //play.js
-
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  stdin.on('data', handleUserInput);
-  return stdin;
-};
-
-const handleUserInput = function(key) {
-  if (key === '\u0003') {
-    process.stdout.write('\nQuitting Game!\n');
-    process.exit();
-  }
-};
-
 const { connect } = require('./client.js');
+const { setupInput } = require('./input.js');
+
 console.log('Connecting ...');
-setupInput();
 connect();
+setupInput();
