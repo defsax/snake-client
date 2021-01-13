c690b34828c926d5815324eb214e64f73a80d0ad
Perry Defayette <defayetteperry@gmail.com>
2021-01-13 18:00:04 +0000

    Added movement commands to client.

--- play.js
+++ play.js
@@ -1,4 +1,22 @@
 //play.js
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function(key) {
+  if (key === '\u0003') {
+    process.stdout.write('\nQuitting Game!\n');
+    process.exit();
+  }
+};
+
 const { connect } = require('./client.js');
 console.log('Connecting ...');
+setupInput();
 connect();
