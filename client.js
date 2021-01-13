3ca0a997ebbc79e8dddbd819df23a421cf60747a
Perry Defayette <defayetteperry@gmail.com>
2021-01-13 17:40:12 +0000

    Added sending server message on connect.

--- client.js
+++ client.js
@@ -9,14 +9,20 @@ const net = require('net');
 
 const connect = function() {
   const conn = net.createConnection({
-    host: '10.0.2.15',
-    port: 50541
+    host: '135.23.222.131',
+    //host: '10.0.2.15',
+    port: 50542
+    //port: 50541
   });
   // interpret incoming data as text
   conn.setEncoding('utf8');
 
   conn.on('data', (data) => {
     console.log('Server says: ', data);
+  });
+  conn.on('connect', (data) => {
+    console.log('Connekted.');
+    conn.write('Name: M0B');
   });
 
   return conn;
